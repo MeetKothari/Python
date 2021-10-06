38bdc78a82ff2fd0256e8cc1bbe3558a6afb955e
Meet Kothari <72781025+MeetKothari@users.noreply.github.com>
2021-10-06 12:38:14 -0400

    Create adder.py

---
+++ Basic Foundations/Basic Arithmetic/adder.py
@@ -0,0 +1,4 @@
+val1 = input("Please enter a number for adding: ")
+val2 = input("Please enter a second number for adding: ")
+val3 = float(val1) + float(val2)
+print('The sum of {0} and {1} is: ', val3)
