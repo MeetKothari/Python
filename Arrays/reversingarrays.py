40404e806daff1f411a2791130f9168e7512d709
Meet Kothari <72781025+MeetKothari@users.noreply.github.com>
2022-01-04 15:26:54 -0500

    Create reversingarrays.py

---
+++ Arrays/reversingarrays.py
@@ -0,0 +1,21 @@
+import array
+
+array = [1,2,3]
+second = array[2]
+
+print (second)
+
+fruit = ['Pears', 'Apples', 'Oranges']
+
+for o in reversed(fruit):
+    print (o)
+
+array.reverse()
+print (array)
+
+a_list = ['a', 'b', 'c', 'd', 'e', 'f']
+print(a_list[1:3])
+print(a_list[:4])
+print(a_list[3:])
+print(a_list[:])
+
