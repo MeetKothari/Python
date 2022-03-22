cee14fcdc683cf0998225e865551b20ec02979fb
Meet Kothari <72781025+MeetKothari@users.noreply.github.com>
2022-03-22 10:39:03 -0400

    Create runtime32ms.py

---
+++ Leetcode/Sqrt(x)/runtime32ms.py
@@ -0,0 +1,12 @@
+# Find the sqrt(x)
+# Return type is int
+# Result is truncated, only return int part
+
+# Runtime: 32 ms, faster than 96.92 % of Python3 online submissions for Sqrt(x).
+# Memory Usage: 13.9 MB, less than 23.79 % of Python3 online submissions for Sqrt(x).
+
+class Solution: 
+    def mySqrt(self, x: int) -> int:
+        #import solution 
+        return int(math.sqrt(x))
+    
