2e4b12401a50145bdda8fcec263c36da07670ec2
Meet Kothari <72781025+MeetKothari@users.noreply.github.com>
2022-06-03 10:24:34 -0400

    Update source.py

--- Automate the Boring Stuff with Python/Password/source.py
+++ Automate the Boring Stuff with Python/Password/source.py
@@ -5,12 +5,12 @@
  
  typedPassword = input()
  
-  if typedPassword == secretPassword:
-    print('Access granted')
-    if typedPassword == '12345':
-        print('That password is one that an idiot puts on their luggage.')
-  else:
-    print('Access denied')
+ if typedPassword == secretPassword: print('Access granted')
+ 
+ if typedPassword == '12345':
+  print('That password is one that an idiot puts on their luggage.')
+ else:
+  print('Access denied')
     
  # Above is a snippet from the Textbook titled, "Automate the Boring Stuff with Python" by Al Sweigart
  # This snippet, taken directly from the source, is a simple Python program that opens a file to compare 
