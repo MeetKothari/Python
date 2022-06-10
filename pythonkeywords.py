2fb558c4513c1d776b770b903bf1517c71f63267
Meet Kothari <72781025+MeetKothari@users.noreply.github.com>
2022-06-10 08:43:58 -0400

    Create pythonkeywords.py

---
+++ pythonkeywords.py
@@ -0,0 +1,8 @@
+# An interesting script I found on https://www.geeksforgeeks.org that displays all of the Python keywords.
+# I didn't even know something like this existed. You learn something new everyday!
+
+import keyword # this is a module used to determine if a string is a keyword or soft keyword.
+ 
+# printing all keywords at once using "kwlist()"
+print("\nThe list of keywords is: ")
+print (keyword.kwlist)
