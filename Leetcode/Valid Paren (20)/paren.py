09ad4b3fb7dc1e4a9d67a48d6b80aeafee924438
Meet Kothari <72781025+MeetKothari@users.noreply.github.com>
2022-07-08 09:22:54 -0400

    Update paren.py

--- Leetcode/Valid Paren (20)/paren.py
+++ Leetcode/Valid Paren (20)/paren.py
@@ -1,18 +1,18 @@
 class Solution:
     def isValid(self, s: str) -> bool:
         
-        stack = []
+        stack = [] # create stack to hold values before matching 
         
-        closetoopen = { ')' : '(', ']' : '[', '}' : '{'}
+        closetoopen = { ')' : '(', ']' : '[', '}' : '{'} # hash to map the closed brackets to open ones
         
-        for c in s:
-            if c in closetoopen:
-                if stack and stack[-1] == closetoopen[c]: stack.pop()
-                else: return False
+        for c in s: # for character in string
+            if c in closetoopen: # if in map
+                if stack and stack[-1] == closetoopen[c]: stack.pop() # pop corresponding close bracket
+                   else: return False   # if can't, return false
             else:
-                stack.append(c)
+                stack.append(c) # otherwise, add c to stack
         
-        return True if not stack else False 
+        return True if not stack else False # return true if empty, else false (empty stack = valid)
         
        
         
