b441b6ee0ada402a1752009eff0142e166950621
Meet Kothari <72781025+MeetKothari@users.noreply.github.com>
2022-07-15 08:24:04 -0400

    Create listcomp.py

---
+++ listcomp.py
@@ -0,0 +1,11 @@
+# A file playing around with list comprehension and tuple manipulation
+
+mylist = [1,2,3,4,5,6,7,8,9]
+
+mysquaredlist = [i * i for i in mylist] # list comp
+
+print(mylist)
+print(mysquaredlist)
+
+mytuple = ("M", "K", "20") # this is a tuple
+print(type(mytuple))
